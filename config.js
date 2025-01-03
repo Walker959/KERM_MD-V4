146697a64c16714bd9b80db7e349ffd56e9fbd4d
Walker959 <pw86861@gmail.com>
2025-01-02 21:24:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "uJZWnRjI#DVIXEYFbnU9fmbhkNXv3bGMCy--4nCkEmyE-uOAUxkM",
+SESSION_ID: process.env.SESSION_ID || "zAAFxD7B#ZE6YaBmNxgn-g8aax9KXy0mbXIE9AM1Gc9g-dTydZsg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
@@ -15,7 +15,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/osriwc.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR IM ONLINE I'M KERM_MD-V4 WHATSAPP BOT\n\n> 𝖡𝖸 𝖪𝖦𝖳𝖤𝖢𝖧",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "😈",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 FAKE_TYPING: process.env.FAKE_TYPING || "true",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
